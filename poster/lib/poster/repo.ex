cb00824b5467efd0d3130bcf3c43c73cfc908b94
Vinícius Müller <vinigm.nho@gmail.com>
2022-08-14 12:30:25 -0300

    fix: ecto repo configuration and db name in compose file

--- poster/lib/poster/repo.ex
+++ poster/lib/poster/repo.ex
@@ -4,27 +4,4 @@ defmodule Poster.Repo do
     adapter: Ecto.Adapters.Postgres
 
   use Scrivener, page_size: 10
-
-  def init(_, config) do
-    config =
-      if Mix.env() != :test do
-        config
-        |> Keyword.put(:username, System.get_env("PGUSER") || "postgres")
-        |> Keyword.put(:password, System.get_env("PGPASSWORD") || "postgres")
-        |> Keyword.put(:database, System.get_env("PGDATABASE") || "poster_dev")
-        |> Keyword.put(:hostname, System.get_env("PGHOST") || "127.0.0.1")
-        |> Keyword.put(:port, get_port())
-      else
-        config
-      end
-
-    {:ok, config}
-  end
-
-  defp get_port() do
-    case System.get_env("PGPORT") do
-      nil -> 5432
-      port -> String.to_integer(port)
-    end
-  end
 end
